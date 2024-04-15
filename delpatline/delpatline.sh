1773c0b5fb5603dfc7fa8fd7bf09ccf5994d16c2
sinankallayi <71578512+sinankallayi@users.noreply.github.com>
2024-04-15 14:49:59 +0530

    Completed

---
+++ delpatline/delpatline.sh
@@ -0,0 +1,18 @@
+if [ $# -ne 2 ]
+then
+echo "Syntax is <$0><string><filename>"
+exit 1
+fi
+term=`tty`
+exec<$2
+while read line
+do
+echo $line | grep -w $1>/dev/null
+if [ $? -ne 0 ]
+then
+echo $line >>temp
+fi
+done
+exec<$term
+mv temp $2
+
